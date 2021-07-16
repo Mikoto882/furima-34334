8b9f44460bd4120b241a3bf2aa02df549132c892
Mikoto882 <hisashi.doraguniru@gmail.com>
2021-07-16 20:24:39 +0900

    ユーザー管理機能修正後（1回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,2 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
-  def index
-  end
-
-  private
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-end
+  before_action :authenticate_user!
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    email                 {Faker::Internet.free_email}
+    password              {'123abc'}
     password_confirmation {password}
-    last_name             {'abc'}
-    first_name            {'abc'}
-    last_name_read        {'abc'}
-    first_name_read       {'abc'}
+    last_name             {'ひらがな'}
+    first_name            {'ひらがな'}
+    last_name_read        {'カタカナ'}
+    first_name_read       {'カタカナ'}
     birthday              {'2000-12-31'} #"2000/12/31"
   end
 end
