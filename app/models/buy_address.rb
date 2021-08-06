cbf07806b155881c6016d9112e41316cee1178b0
Mikoto882 <hisashi.doraguniru@gmail.com>
2021-08-06 15:00:49 +0900

    Formオブジェクトの作成

---
+++ app/models/buy_address.rb
@@ -0,0 +1,21 @@
+class BuyAddress
+  includ ActiveModel::Model
+  attr_accessor :post_number, :ship_area, :address_city, :address_no, :address_build, :tele_number, :buy_id, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :ship_area
+    validates :address_city
+    validates :address_no
+    validates :tele_number, length: {maximum: 11}
+    validates :buy_id
+    validates :item_id
+    validates :user_id
+  end
+  validates :address_build
+
+  def save
+    buy = Buy.create(item_id: item_id, user_id: user_id)
+    Address.create(post_number: post_number, ship_area: ship_area, address_city: address_city, address_no: address_no, address_build: address_build, tele_number: tele_number, buy_id: buy_id)
+  end
+end
