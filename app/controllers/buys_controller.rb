48e71b80c7ef764e3c565eea8a7365eb6babc84b
Mikoto882 <hisashi.doraguniru@gmail.com>
2021-08-10 21:00:46 +0900

    フォーム情報の保存

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,6 +7,7 @@ class BuysController < ApplicationController
 
   def create
     @buy_address = BuyAddress.new(buy_params)
+    @item = Item.find(params[:item_id])
     if @buy_address.valid?
       @buy_address.save
       redirect_to root_path
@@ -18,7 +19,7 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_address).permit(:post_number, :ship_area_id, :address_city, :address_no, :address_build, :tele_number).marge(item_id: current_item.id, user_id: current_user.id)
+    params.permit(:post_number, :ship_area_id, :address_city, :address_no, :address_build, :tele_number, :item_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,10 +1,10 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :post_number, :ship_area, :address_city, :address_no, :address_build, :tele_number, :buy_id, :item_id, :user_id
+  attr_accessor :post_number, :ship_area_id, :address_city, :address_no, :address_build, :tele_number, :buy_id, :item_id, :user_id
 
   with_options presence: true do
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :ship_area
+    validates :ship_area_id
     validates :address_city
     validates :address_no
     validates :tele_number, length: {maximum: 11}
@@ -15,6 +15,6 @@ class BuyAddress
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    Address.create(post_number: post_number, ship_area: ship_area, address_city: address_city, address_no: address_no, address_build: address_build, tele_number: tele_number, buy_id: buy_id)
+    Address.create(post_number: post_number, ship_area_id: ship_area_id, address_city: address_city, address_no: address_no, address_build: address_build, tele_number: tele_number, buy_id: buy.id)
   end
 end
