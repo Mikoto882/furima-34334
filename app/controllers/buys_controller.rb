3bfa2fb41334f6d489e27e1fe3bedadcac435b7a
Mikoto882 <hisashi.doraguniru@gmail.com>
2021-08-13 10:37:05 +0900

    決済機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,14 +1,14 @@
 class BuysController < ApplicationController
+  before_action :set_buy, only:[:index, :create]
 
   def index
     @buy_address = BuyAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @buy_address = BuyAddress.new(buy_params)
-    @item = Item.find(params[:item_id])
     if @buy_address.valid?
+      pay_buy
       @buy_address.save
       redirect_to root_path
     else
@@ -21,5 +21,18 @@ class BuysController < ApplicationController
   def buy_params
     params.permit(:post_number, :ship_area_id, :address_city, :address_no, :address_build, :tele_number, :item_id).merge(user_id: current_user.id, token: params[:token])
   end
+
+  def set_buy
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_buy
+    Payjp.api_key = "sk_test_8dc05065fab1c305421dd2e2"
+    Payjp::Charge.create(
+      amount: @item.money,
+      card: buy_params[:token],
+      currency: 'jpy'
+    )
+end
 
 end
